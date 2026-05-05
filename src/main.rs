1ba17cbfe348081e3fa3fe8344f6696e40dd964f
Silvio Meneguzzo <meneguzzosilvio@gmail.com>
2026-05-05 11:26:42 +0200

    add ping + mdns

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
 use std::time::Duration;
 
-use libp2p::{Multiaddr, futures::StreamExt, noise, ping, swarm::SwarmEvent, tcp, yamux};
+use libp2p::{
+    Multiaddr, futures::StreamExt, mdns, noise, ping,
+    swarm::{NetworkBehaviour, SwarmEvent},
+    tcp, yamux,
+};
+
+#[derive(NetworkBehaviour)]
+struct Behaviour {
+    ping: ping::Behaviour,
+    mdns: mdns::tokio::Behaviour,
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -11,11 +21,21 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             noise::Config::new,
             yamux::Config::default,
         )?
-        .with_behaviour(|_| ping::Behaviour::default())?
+        .with_behaviour(|key| -> Result<_, Box<dyn std::error::Error + Send + Sync>> {
+            Ok(Behaviour {
+                ping: ping::Behaviour::default(),
+                mdns: mdns::tokio::Behaviour::new(
+                    mdns::Config::default(),
+                    key.public().to_peer_id(),
+                )?,
+            })
+        })?
         .with_swarm_config(|config| {
             config.with_idle_connection_timeout(Duration::from_secs(u64::MAX))
         })
         .build();
+
+    println!("Local peer id: {}", swarm.local_peer_id());
 
     swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
 
@@ -35,7 +55,20 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             SwarmEvent::ConnectionClosed { peer_id, .. } => {
                 println!("Connection closed with {}", peer_id)
             }
-            SwarmEvent::Behaviour(event) => println!("{:?}", event),
+            SwarmEvent::Behaviour(BehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
+                for (peer_id, addr) in list {
+                    println!("mDNS discovered {peer_id} at {addr}");
+                    let _ = swarm.dial(addr);
+                }
+            }
+            SwarmEvent::Behaviour(BehaviourEvent::Mdns(mdns::Event::Expired(list))) => {
+                for (peer_id, _) in list {
+                    println!("mDNS expired {peer_id}");
+                }
+            }
+            SwarmEvent::Behaviour(BehaviourEvent::Ping(event)) => {
+                println!("{:?}", event)
+            }
             _ => {}
         }
     }
